a523f8eba9d13652bfc192df11cf1b9372f18a56
Antonio Petricca <antonio.petricca@gmail.com>
2023-10-27 16:38:01 +0200

    Indentation.

--- plugin/src/main/java/it/antoniopetricca/maven/plugins/filemerger/MavenFileMergerMojo.java
+++ plugin/src/main/java/it/antoniopetricca/maven/plugins/filemerger/MavenFileMergerMojo.java
@@ -2,6 +2,7 @@ package it.antoniopetricca.maven.plugins.filemerger;
 
 // https://www.baeldung.com/maven-plugin
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -14,6 +15,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mojo(
     defaultPhase = LifecyclePhase.PROCESS_RESOURCES,
@@ -37,6 +41,15 @@ public class MavenFileMergerMojo extends AbstractMojo {
         );
     }
 
+    private List<String> getFileLines(File file, Charset charset)
+        throws IOException
+    {
+        return Files.readAllLines(
+            file.toPath(),
+            charset
+        );
+    }
+
     private void mergeTargetFile(TargetFileConfiguration targetFileConfiguration)
         throws IOException
     {
@@ -51,7 +64,13 @@ public class MavenFileMergerMojo extends AbstractMojo {
             targetFileConfiguration.getCharset()
         );
 
-        String targetFileContent = getFileContent(targetFile, charset);
+        String  targetFileContent = getFileContent(targetFile, charset);
+        Integer indentation       = targetFileConfiguration.getIndentation();
+        String  indentationString = null;
+
+        if (indentation > 0) {
+            indentationString = StringUtils.leftPad("", indentation, " ");
+        }
 
         for (SourceFileConfiguration sourceFileConfiguration : targetFileConfiguration.getSourceFiles()) {
             File sourceFile = sourceFileConfiguration.getFile();
@@ -61,13 +80,32 @@ public class MavenFileMergerMojo extends AbstractMojo {
                 sourceFile.toString()
             ));
 
-            String sourceFileContent = getFileContent(sourceFile, charset);
+            String sourceFileContent;
+
+            if (null != indentationString) {
+                List<String> sourceFileLines   = getFileLines(sourceFile, charset);
+                final String streamIndentation = indentationString;
+
+                sourceFileContent = sourceFileLines
+                    .stream()
+                    .map(line -> (streamIndentation + line))
+                    .collect(Collectors.joining("\n"));
+            } else {
+                sourceFileContent = getFileContent(sourceFile, charset);
+            }
 
             targetFileContent = targetFileContent.replaceAll(
                 sourceFileConfiguration.getPlaceholder(),
                 sourceFileContent
             );
         }
+
+        log.info("Writing merged target file...");
+
+        Files.write(
+            targetFile.toPath(),
+            targetFileContent.getBytes(charset)
+        );
 
         log.info("Done.");
     }
