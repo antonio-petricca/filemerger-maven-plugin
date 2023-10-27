566786e93459c6a70c3285950c2d19012893d1e2
Antonio Petricca <antonio.petricca@gmail.com>
2023-10-28 00:19:10 +0200

    Package refactoring.

--- tester/src/main/java/io/github/antoniopetricca/maven/plugins/filemerger/MavenFileMergerTester.java
+++
@@ -1,9 +0,0 @@
-package io.github.antoniopetricca.maven.plugins.filemerger;
-
-public class MavenFileMergerTester {
-
-    public void main() {
-        System.out.println("Happy days!");
-    }
-
-}
